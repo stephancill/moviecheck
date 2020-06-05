6d193bbe059448802941504976af1439c6613390
Stephan Cilliers <stephanus.cilliers@protonmail.com>
2020-06-05 21:16:25 +0200

    Scroll back button

--- static/js/base.js
+++ static/js/base.js
@@ -2,17 +2,50 @@ $(document).ready(function() {
     $(window).trigger('resize');
 })
 
-$(".list-row .scroll-button").click(function() {
+jQuery.fn.visible = function() {
+    return this.css('visibility', 'visible');
+};
+
+jQuery.fn.invisible = function() {
+    return this.css('visibility', 'hidden');
+};
+
+$(".list-row").scroll(function() {
+    if ($(".list-row").scrollLeft() > $(".list-row .filler").width()) {
+        $(".list-row .scroll-button.left").visible()
+    } else {
+        $(".list-row .scroll-button.left").invisible()
+    }
+
+    if (($(".list-row .movie").width() + 46) * $(".list-row .movie").length + 
+    $(".list-row .filler").width() + $(".list-row .filler-end").width() < 
+    $(".list-row").scrollLeft() + $(".list-row").width()) {
+        $(".list-row .scroll-button.right").invisible()
+    } else {
+        $(".list-row .scroll-button.right").visible()
+    }
+})
+
+$(".list-row .scroll-button.right").click(function() {
     $(".list-row")[0].scrollBy({
         left: 236,
         behavior: 'smooth'
     })
 })
 
+$(".list-row .scroll-button.left").click(function() {
+    $(".list-row")[0].scrollBy({
+        left: -236,
+        behavior: 'smooth'
+    })
+})
+
 $(window).resize(function() {
-    if ($(".list-row .movie").width() * $(".list-row .movie").length + $(".list-row .filler").width() > $(".list-row").width()) {
-        $(".list-row .scroll-button").show()
+    $(".list-row .filler").css("min-width", ($(window).width() - $(".container").width())/2 - 40)
+
+    if (($(".list-row .movie").width() + 46) * $(".list-row .movie").length + $(".list-row .filler").width() > $(".list-row").width()) {
+        $(".list-row .scroll-button.right").visible()
     } else {
-        $(".list-row .scroll-button").hide()
+        $(".list-row .scroll-button.right").invisible()
     }
 }) 
